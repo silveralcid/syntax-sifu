ea8ed244bf0807bd468d60f670c63946d7c92354
Silver Alcid <silveralcid@gmail.com>
2025-09-27 19:32:37 -0400

    feat: enhance layout of Home component with responsive grid and flex properties

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,14 +1,19 @@
 export default function Home() {
   return (
     <main className="">
-
-<div className="flex items-center justify-center min-h-screen">
-  <div className="grid grid-cols-5 grid-rows-5 gap-4">
-    <div className="col-span-2 row-span-4 bg-red-300">1</div>
-    <div className="col-span-3 row-span-5 col-start-3 bg-green-300">2</div>
-    <div className="col-span-2 row-start-5 bg-blue-300">3</div>
-  </div>
-</div>
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="grid grid-cols-5 grid-rows-5 gap-4 w-screen h-screen max-w-6xl max-h-[90vh]">
+          <div className="col-span-2 row-span-4 bg-red-300 flex items-center justify-center">
+            1
+          </div>
+          <div className="col-span-3 row-span-5 col-start-3 bg-green-300 flex items-center justify-center">
+            2
+          </div>
+          <div className="col-span-2 row-start-5 bg-blue-300 flex items-center justify-center">
+            3
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
