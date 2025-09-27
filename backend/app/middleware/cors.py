eb041883fc74ead180885938f43ec0d2e5d56daa
Silver Alcid <silveralcid@gmail.com>
2025-09-27 10:24:25 -0400

    feat: update CORS middleware to specify allowed origins

--- backend/app/middleware/cors.py
+++ backend/app/middleware/cors.py
@@ -1,10 +1,15 @@
 # backend/app/middleware/cors.py
 from fastapi.middleware.cors import CORSMiddleware
 
+
 def add_cors(app):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
+        allow_origins=[
+            "https://syntax-sifu.vercel.app",
+            "https://www.syntaxsifu.com"
+            "http://localhost:3000"
+        ],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
