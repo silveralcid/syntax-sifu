c80bbd56e357ebddcc2dd901f207eb28003975a8
Silver Alcid <silveralcid@gmail.com>
2025-09-27 06:24:07 -0400

    chore: load environment variables using dotenv

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from app.routes import challenge, submit
 from app.middleware.rate_limiter import rate_limiter
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 app = FastAPI(title="Syntax Sifu API")
 app.middleware("http")(rate_limiter)
